fb2eaf8750d94cdcdfead5326f142de568ed8085
Dvdolivera <olivera.david.antonio@gmail.com>
2021-12-09 20:47:05 -0300

    update
    update

--- src/ui/page-content.js
+++ src/ui/page-content.js
@@ -6,48 +6,50 @@ import download from "./sections/download-section.js"
 
 
 export function addInitialEventListeners() {
-    document.querySelector("#aboutbutton").addEventListener("click",incrustarAbout);
-    document.querySelector("#homebutton").addEventListener("click",incrustarHome);
-    document.querySelector("#contactbutton").addEventListener("click",incrustarContact);
-    document.querySelector("#blogbutton").addEventListener("click",incrustarBlog);
+    document.querySelector("#aboutbutton").addEventListener("click",manejarSeleccion);
+    document.querySelector("#homebutton").addEventListener("click",manejarSeleccion);
+    document.querySelector("#contactbutton").addEventListener("click",manejarSeleccion);
+    document.querySelector("#blogbutton").addEventListener("click",manejarSeleccion);
     document.querySelectorAll(".botonrequest").forEach((boton => {
-        boton.addEventListener("click",IncrustarDownload)
+        boton.addEventListener("click",manejarSeleccion)
     }));
-    document.querySelector("#mobile-home").addEventListener("click",incrustarHome);
-    document.querySelector("#mobile-about").addEventListener("click",incrustarAbout);
-    document.querySelector("#mobile-contact").addEventListener("click",incrustarContact);
-    document.querySelector("#mobile-blog").addEventListener("click",incrustarBlog);
+    document.querySelector("#mobile-home").addEventListener("click",manejarSeleccion);
+    document.querySelector("#mobile-about").addEventListener("click",manejarSeleccion);
+    document.querySelector("#mobile-contact").addEventListener("click",manejarSeleccion);
+    document.querySelector("#mobile-blog").addEventListener("click",manejarSeleccion);
     
     
 
 }
+function manejarSeleccion(e){
+    let keyword = e.target.dataset.seccion;
 
-function IncrustarDownload(){
-    console.log('funciona');
-    document.querySelector("#page-content").innerHTML = download;
+    if(keyword === "home"){
+        incrustarSeccion(home)
+    };
+    
+    if(keyword === "about"){
+        incrustarSeccion(about)
+    };
+
+    if(keyword === "download"){
+        incrustarSeccion(download)
+    };
+    
+    if(keyword === "contact"){
+        incrustarSeccion(contact)
+    };
+
+    if(keyword === "blog"){
+        incrustarSeccion(blog)
+    };
+
 
 }
 
-function incrustarAbout() {  
+export function incrustarSeccion(seccion){
    
-    document.querySelector("#page-content").innerHTML = about;
+    document.querySelector("#page-content").innerHTML = seccion;
 
-}
+};
 
-function incrustarHome() {  
-   
-    document.querySelector("#page-content").innerHTML = home;
-
-}
-
-function incrustarContact() {  
-   
-    document.querySelector("#page-content").innerHTML = contact;
-
-}
-
-function incrustarBlog() {  
-   
-    document.querySelector("#page-content").innerHTML = blog;
-
-}
---
+++ src/ui/page-content.test.js
@@ -0,0 +1,11 @@
+import  {addInitialEventListeners,incrustarSeccion} from "./page-content"
+import about from "./sections/about-section.js"
+import home from "./sections/home-section.js"
+import contact from "./sections/contact-section.js"
+import blog from "./sections/blog-section.js"
+import download from "./sections/download-section.js"
+
+test('Primer testeo en Jest',()=>{
+    
+});
+
